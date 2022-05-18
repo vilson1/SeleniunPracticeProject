1b5de3fda1043cbc278335cc2b776c5defc099d2
User <vilson.shehu@outlook.com>
2022-05-18 09:21:09 +0200

    Day 5

---
+++ src/test/java/com/cydeo/tests/day5/TestNg_Intro.java
@@ -0,0 +1,34 @@
+package com.cydeo.tests.day5;
+
+import org.testng.annotations.*;
+
+public class TestNg_Intro {
+
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("Before class is running");
+    }
+    @AfterClass
+    public void teaeDownClass(){
+        System.out.println("After class is running");
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("before method is running...");
+    }
+    @AfterMethod
+    public void tearDownMethods(){
+        System.out.println(".........after method is running");
+    }
+
+    @Test
+    public void test1(){
+
+        System.out.println("Test 1 is running");
+    }
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is running");
+    }
+}
