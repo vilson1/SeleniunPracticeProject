e46ec481393516f66bba81e24ea5a6df8518cef5
User <vilson.shehu@outlook.com>
2022-06-06 09:26:39 +0200

    Day 9

--- src/test/java/com/cydeo/tests/day9/SingletonPractice.java
+++ src/test/java/com/cydeo/tests/day9/SingletonPractice.java
@@ -3,16 +3,23 @@ package com.cydeo.tests.day9;
 import org.testng.annotations.Test;
 
 public class SingletonPractice {
-    @Test
+    @Test(priority = 2)
     public void singleton_practice(){
         String str=Singleton.word();
         System.out.println("str = " + str);
 
         String str2=Singleton.word();
-        System.out.println("str = " + str2);
+        System.out.println("str2 = " + str2);
 
         String str3=Singleton.word();
-        System.out.println("str = " + str3);
+        System.out.println("str3 = " + str3);
 
+
+
+    }
+    @Test(priority = 1)
+    public void singleton_practice2() {
+        String str4= Singleton.word();
+        System.out.println("str4 = " + str4);
     }
 }
